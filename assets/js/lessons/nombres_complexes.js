8fe4bc08d876f37199e1bdd16b39656d64a67505
Skyost <me@skyost.eu>
2017-02-01 16:04:37 +0100

    Nouveau cours sur les nombres complexes

---
+++ assets/js/lessons/nombres_complexes.js
@@ -0,0 +1,62 @@
+$(document).ready(function() {
+	createPlots(computeDimension());
+});
+
+$(window).resize(function() {
+	createPlots(computeDimension());
+});
+
+function createPlots(dimensions) {
+	functionPlot({
+		title: 'Représentation de z',
+		target: '#representation-1',
+		grid: true,
+		height: dimensions.height,
+		width: dimensions.width,
+		data: [
+			{
+				points: [
+					[1, 1]
+				],
+				fnType: 'points',
+				graphType: 'scatter'
+			},
+			{
+				title: 'M(1; 1)',
+				fn: '1',
+				range: [1, 1]
+			}
+		]
+	});
+	
+	functionPlot({
+		title: 'Représentation de z\'',
+		target: '#representation-2',
+		grid: true,
+		height: dimensions.height,
+		width: dimensions.width,
+		data: [
+			{
+				fn: 'x * x + y * y - 2',
+				fnType: 'implicit'
+			},
+			{
+				points: [
+					[1, 1]
+				],
+				fnType: 'points',
+				graphType: 'scatter'
+			},
+			{
+				title: 'Module de z\'',
+				fn: 'x',
+				range: [0, 1]
+			},
+			{
+				title: 'M(1; 1)',
+				fn: '1',
+				range: [1, 1]
+			}
+		]
+	});
+}
