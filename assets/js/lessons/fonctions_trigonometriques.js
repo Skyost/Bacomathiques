a66d17e8f15a5a1b3d1ae69f04826f8cb3f61463
Skyost <me@skyost.eu>
2017-03-05 00:44:45 +0100

    Ajout d'un cours

---
+++ assets/js/lessons/fonctions_trigonometriques.js
@@ -0,0 +1,101 @@
+$(document).ready(function() {
+	createPlots(computeDimension());
+});
+
+$(window).resize(function() {
+	createPlots(computeDimension());
+});
+
+function createPlots(dimensions) {
+	functionPlot({
+		title: 'Repère trigonométrique',
+		target: '#representation-1',
+		grid: true,
+		height: dimensions.height,
+		width: dimensions.width,
+		xAxis: {domain: [-3, 3]},
+		data: [
+			{
+				fn: 'x * x + y * y - 1',
+				fnType: 'implicit'
+			},
+			{
+				points: [
+					[0, 1],
+					[1, 0]
+				],
+				fnType: 'points',
+				graphType: 'scatter'
+			},
+			{
+				title: 'J(0; 1)',
+				fn: '1',
+				range: [0, 0]
+			},
+			{
+				title: 'I(1; 0)',
+				fn: '0',
+				range: [1, 1]
+			},
+			{
+				vector: [0, 1],
+				offset: [0, 0],
+				graphType: 'polyline',
+				fnType: 'vector',
+				color: 'red'
+			},
+			{
+				vector: [1, 0],
+				offset: [0, 0],
+				graphType: 'polyline',
+				fnType: 'vector',
+				color: 'red'
+			}
+		]
+	});
+	
+	x = Math.cos(Math.PI / 4); // cos(pi/4) = sin(pi/4) = sqrt(2) / 2
+	
+	functionPlot({
+		title: 'cos(x + pi) = -cos(x)',
+		target: '#representation-2',
+		grid: true,
+		height: dimensions.height,
+		width: dimensions.width,
+		xAxis: {domain: [-3, 3]},
+		data: [
+			{
+				fn: 'x * x + y * y - 1',
+				fnType: 'implicit'
+			},
+			{
+				points: [
+					[x, x],
+					[-x, x]
+				],
+				fnType: 'points',
+				graphType: 'scatter'
+			},
+			{
+				title: 'Distance OM',
+				fn: 'x',
+				range: [0, x]
+			},
+			{
+				title: 'Distance OM\'',
+				fn: '-x',
+				range: [-x, 0]
+			},
+			{
+				title: 'M(x; y)',
+				fn: 'cos(PI / 4)',
+				range: [x, x]
+			},
+			{
+				title: 'M\'(x + pi; y + pi)',
+				fn: 'sin(PI / 4)',
+				range: [-x, -x]
+			},
+		]
+	});
+}
