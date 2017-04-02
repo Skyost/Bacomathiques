3a45ebbe56127d90007efaa9e0f6cf6e8efdc74a
Skyost <me@skyost.eu>
2017-04-02 17:05:24 +0200

    Correction de quelques problèmes

--- assets/js/maths.js
+++ assets/js/maths.js
@@ -1,29 +1,6 @@
 var graphHeight = 400;
 var graphWidth = 600;
 
-$(document).ready(function() {
-	MathJax.Hub.Config({
-		tex2jax: {
-			inlineMath: [
-				['$','$'],
-				['\\(','\\)']
-			]
-		},
-		'HTML-CSS': {
-			scale: 90,
-			webFont: 'Asana-Math',
-			preferredFont: 'Asana-Math', 
-			availableFonts: [
-				'Asana-Math',
-				'TeX'
-			], 
-			extensions: ['handle-floats.js']
-		},
-		messageStyle: 'none',
-		showMathMenu: false
-	});
-});
-
 function computeDimension() {
 	var height = graphHeight;
 	var width = graphWidth;
