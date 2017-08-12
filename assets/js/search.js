d1871096caca2ed7b46fa4a6474b6c4f96ea2c92
Skyost <me@skyost.eu>
2017-08-12 19:54:56 +0200

    Correction de bugs

--- assets/js/search.js
+++ assets/js/search.js
@@ -57,12 +57,12 @@ function displaySearchResults(results, pages) {
 
 		for(var i = 0; i < results.length; i++) {
 			var item = pages[results[i].ref];
-			appendString += '<div class="col-xs-12 col-sm-6 col-md-4">';
-			appendString += '<div class="card">';
-			appendString += '<div class="card-block">';
-			appendString += '<h2>' + item.title + '</h2>';
+			appendString += '<div class="col-xs-12 col-md-6 col-lg-4">';
+			appendString += '<div class="card bg-light">';
+			appendString += '<div class="card-header d-flex align-items-center"><h2 class="w-100">' + item.title + '</h2></div>';
+			appendString += '<div class="card-body">';
 			appendString += '<p class="card-text">' + item.excerpt + '</p>';
-			appendString += '<a href="' + item.url + '" class="btn btn-primary">Lien vers la page</a>';
+			appendString += '<a href="' + item.url + '" class="btn btn-primary pull-right">Lien vers la page <i class="fa fa-arrow-right" aria-hidden="true"></i></a>';
 			appendString += '</div>';
 			appendString += '</div>';
 			appendString += '</div>';
@@ -71,7 +71,7 @@ function displaySearchResults(results, pages) {
 		searchResults.html(appendString);
 		searchResults.css('display', '');
 		
-		$('.card-block h2').matchHeight();
+		$('.card-header').matchHeight();
 		$('.card-text').matchHeight();
 	}
 	$('.fa-spinner').css('display', 'none');
