021dfe782806eb5c391794746ecf52c616a72881
Skyost <me@skyost.eu>
2019-04-06 12:51:54 +0200

    Mises à jour dans le rendu pour les tablettes.

--- lib/util/util.dart
+++ lib/util/util.dart
@@ -1,4 +1,5 @@
 import 'dart:io';
+import 'dart:math';
 
 import 'package:bacomathiques/app/app.dart';
 import 'package:flutter/material.dart';
@@ -37,6 +38,12 @@ Future<String> getAndSave(String file, String key, String url) async {
   }
 
   return content;
+}
+
+/// Returns whether the current screen is a tablet.
+bool isTablet(Size screenSize) {
+  double diagonal = sqrt(pow(screenSize.width, 2) + pow(screenSize.height, 2));
+  return diagonal > 1100 || screenSize.shortestSide >= 600;
 }
 
 /// A centered circular progress indicator.
