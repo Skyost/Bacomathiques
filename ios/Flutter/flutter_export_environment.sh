9bbeca913e944034881e9bd6b45215399cc39f25
Skyost <me@skyost.eu>
2020-05-17 17:26:21 +0200

    Quelques mises à jour pour iOS.

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
-export "FLUTTER_ROOT=D:\Hugo\Programmes\Flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Hugo\Projets\Flutter\bacomathiques"
-export "FLUTTER_TARGET=lib\main.dart"
+export "FLUTTER_ROOT=/Users/hugo/Documents/flutter"
+export "FLUTTER_APPLICATION_PATH=/Users/hugo/Documents/Bacomathiques"
+export "FLUTTER_TARGET=lib/main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "SYMROOT=${SOURCE_ROOT}/../build\ios"
+export "SYMROOT=${SOURCE_ROOT}/../build/ios"
 export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
-export "FLUTTER_FRAMEWORK_DIR=D:\Hugo\Programmes\Flutter\bin\cache\artifacts\engine\ios"
-export "FLUTTER_BUILD_NAME=1.2.1"
-export "FLUTTER_BUILD_NUMBER=28"
+export "FLUTTER_FRAMEWORK_DIR=/Users/hugo/Documents/flutter/bin/cache/artifacts/engine/ios-release"
+export "FLUTTER_BUILD_NAME=1.2.2"
+export "FLUTTER_BUILD_NUMBER=29"
