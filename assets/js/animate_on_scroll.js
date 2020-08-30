fdac80ae0dcd07758f2f71f8bf5a2773f49092c1
Skyost <me@skyost.eu>
2020-08-31 00:17:11 +0200

    Quelques corrections.

--- assets/js/animate_on_scroll.js
+++ assets/js/animate_on_scroll.js
@@ -1,3 +1,7 @@
 $(document).ready(function () {
-    sal();
+    let width = $(window).width();
+    let threshold = width <= 768 ? 0.1 : 0.5;
+    sal({
+        threshold: threshold,
+    });
 });
