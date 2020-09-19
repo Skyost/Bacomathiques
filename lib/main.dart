f0ba4076fe45b05ebd56961fe812865ff1ed294a
Skyost <me@skyost.eu>
2020-09-19 13:21:29 +0200

    Requête de l'autorisation de tracking sur iOS.

--- lib/main.dart
+++ lib/main.dart
@@ -38,42 +38,55 @@ void main() async {
 }
 
 /// The app main class.
-class BacomathiquesApp extends StatelessWidget {
+class BacomathiquesApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() => _BacomathiquesAppState();
+
+}
+
+/// The app main class state.
+class _BacomathiquesAppState extends State<BacomathiquesApp> {
+  @override
+  void initState() {
+    super.initState();
+    Admob.requestTrackingAuthorization();
+  }
+
   @override
   Widget build(BuildContext context) => MultiProvider(
-        providers: [
-          Provider<Server>(
-            create: (_) => Server()..start(),
-            dispose: (_, server) => server.close(),
-            lazy: false,
-          ),
-          ChangeNotifierProvider<SettingsModel>(
-            create: (_) => SettingsModel()..load(context),
-            lazy: false,
-          ),
-        ],
-        child: Consumer<SettingsModel>(
-          builder: (context, settings, _) => MaterialApp(
-            title: App.APP_NAME,
-            theme: settings.appTheme.flutterThemeData,
-            initialRoute: '/',
-            routes: {
-              '/': (context) => HomePage(),
-              '/levels': (context) => LevelsPage(),
-              '/lessons': (context) {
-                Map<String, dynamic> arguments = ModalRoute.of(context).settings.arguments;
-                return LessonsPage(endpoint: arguments['endpoint']);
-              },
-              '/html': (context) {
-                Map<String, dynamic> arguments = ModalRoute.of(context).settings.arguments;
-                return AdMobHTMLPage(endpoint: arguments['endpoint'], anchor: arguments['anchor']);
-              },
-              '/comments': (context) {
-                Map<String, dynamic> arguments = ModalRoute.of(context).settings.arguments;
-                return CommentsPage(endpoint: arguments['endpoint']);
-              },
-            },
-          ),
-        ),
-      );
+    providers: [
+      Provider<Server>(
+        create: (_) => Server()..start(),
+        dispose: (_, server) => server.close(),
+        lazy: false,
+      ),
+      ChangeNotifierProvider<SettingsModel>(
+        create: (_) => SettingsModel()..load(context),
+        lazy: false,
+      ),
+    ],
+    child: Consumer<SettingsModel>(
+      builder: (context, settings, _) => MaterialApp(
+        title: App.APP_NAME,
+        theme: settings.appTheme.flutterThemeData,
+        initialRoute: '/',
+        routes: {
+          '/': (context) => HomePage(),
+          '/levels': (context) => LevelsPage(),
+          '/lessons': (context) {
+            Map<String, dynamic> arguments = ModalRoute.of(context).settings.arguments;
+            return LessonsPage(endpoint: arguments['endpoint']);
+          },
+          '/html': (context) {
+            Map<String, dynamic> arguments = ModalRoute.of(context).settings.arguments;
+            return AdMobHTMLPage(endpoint: arguments['endpoint'], anchor: arguments['anchor']);
+          },
+          '/comments': (context) {
+            Map<String, dynamic> arguments = ModalRoute.of(context).settings.arguments;
+            return CommentsPage(endpoint: arguments['endpoint']);
+          },
+        },
+      ),
+    ),
+  );
 }
