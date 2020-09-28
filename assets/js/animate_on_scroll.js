e70d538a5307818150b6c6966af4c9ccd53ab4f0
Skyost <me@skyost.eu>
2020-09-28 22:06:21 +0200

    Ajout du cours sur les nombres complexes.

--- assets/js/animate_on_scroll.js
+++ assets/js/animate_on_scroll.js
@@ -1,6 +1,6 @@
 $(document).ready(function () {
     let width = $(window).width();
-    let threshold = width <= 768 ? 0.1 : 0.5;
+    let threshold = width <= 768 ? 0.1 : 0.25;
     sal({
         threshold: threshold,
     });
