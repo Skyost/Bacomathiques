efd8c4f62b4bf1cdf8c06ba2d2494f27f9a3f62d
Hugo Delaunay <Skyost@users.noreply.github.com>
2024-01-06 16:44:17 +0100

    Correction du plugin Bootstrap.

--- plugins/bootstrap.client.ts
+++ plugins/bootstrap.client.ts
@@ -1,6 +1,6 @@
-import * as collapse from 'bootstrap/js/dist/collapse'
-import * as modal from 'bootstrap/js/dist/modal'
-import * as carousel from 'bootstrap/js/dist/carousel'
-import * as dropdown from 'bootstrap/js/dist/dropdown'
+import collapse from 'bootstrap/js/dist/collapse'
+import modal from 'bootstrap/js/dist/modal'
+import carousel from 'bootstrap/js/dist/carousel'
+import dropdown from 'bootstrap/js/dist/dropdown'
 
 export default defineNuxtPlugin(nuxtApp => nuxtApp.provide('bootstrap', { collapse, modal, carousel, dropdown }))
